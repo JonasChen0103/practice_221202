71319900e7bb2e6e8c66ffc30ee2bddc09fc1adc
Jonas Chen <pboss.chen@gmail.com>
2022-12-02 15:34:06 +0800

    Add project files.

---
+++ practice_221202/App.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practice_221202
+{
+    class App
+    {
+        private string Name;
+        public string Description;
+
+        public App(string Name, string Description)
+        {
+            this.Name = Name;
+            this.Description = Description;
+        }
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public void SetDescription(string Description)
+        {
+            this.Description = Description;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetDescription()
+        {
+            return Description;
+        }
+
+        public string ReadMail()
+        {
+            return (Name + " reading mails");
+        }
+
+        public string WriteMail()
+        {
+            return (Name + " writing mails");
+        }
+    }
+}
---
+++ practice_221202/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace practice_221202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter \"WebApp\" or \"MobileApp\".");
+            string appType = Console.ReadLine();
+
+            Console.WriteLine("Please enter \"ReadMail\" or \"WriteMail\".");
+            string order = Console.ReadLine();
+
+            if (appType == "WebApp")
+            {
+                App WebApp;
+                WebApp = new App("WebApp", "WebApp's description");
+
+                if (order == "ReadMail")
+                {
+                    Console.WriteLine(WebApp.ReadMail());
+                }
+                else if (order == "WriteMail")
+                {
+                    Console.WriteLine(WebApp.WriteMail());
+                }
+            }
+            else if (appType == "MobileApp")
+            {
+                App MobileApp;
+                MobileApp = new App("MobileApp", "MobileApp's description");
+
+                if (order == "ReadMail")
+                {
+                    Console.WriteLine(MobileApp.ReadMail());
+                }
+                else if (order == "WriteMail")
+                {
+                    Console.WriteLine(MobileApp.WriteMail());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error.");
+            }
+        }
+    }
+}
